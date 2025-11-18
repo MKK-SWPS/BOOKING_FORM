bd866b6bc3db322fc722b6cc1b3854e29b1dc009
Michał Król <m3krol@gmail.com>
2025-11-18 16:13:34 +0000

    Adjust date helper placement

--- index.js
+++ index.js
@@ -597,8 +597,8 @@ app.get('/', (req, res) => {
                 <div class="form-section">
                     <h2>Wybierz datę</h2>
                     <div class="date-picker-container">
-                      <input type="date" id="datePicker" required aria-describedby="dateHelp" min="2025-11-24" max="2025-12-05">
                       <div class="date-help" id="dateHelp">💡 Kliknij ikonę kalendarza po prawej stronie, aby wybrać datę.</div>
+                      <input type="date" id="datePicker" required aria-describedby="dateHelp" min="2025-11-24" max="2025-12-05">
                     </div>
                 </div>
                 
@@ -732,7 +732,7 @@ app.get('/', (req, res) => {
                     }
 
                     if (dateHelp) {
-                      dateHelp.textContent = 'Terminy dostępne od ' + formatDateForDisplay(data.minDate) + ' do ' + formatDateForDisplay(data.maxDate) + '. 💡 Kliknij ikonę kalendarza po prawej stronie, aby wybrać datę.';
+                      dateHelp.textContent = '💡 Kliknij ikonę kalendarza po prawej stronie, aby wybrać datę. Terminy dostępne od ' + formatDateForDisplay(data.minDate) + ' do ' + formatDateForDisplay(data.maxDate) + '.';
                     }
 
                     renderTimeSlots();
